d104eea83add95ea717b6cb32f07c10125a1ef51
jsc <jsanchezcriado@gmail.com>
2020-04-11 01:38:06 +0200

    created api file to interact with the _DATA file

---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+import {
+    _getUsers,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer
+} from './_DATA';
+
+export const getInitialData = () => {
+    return Promise.all([_getUsers, _getQuestions])
+        .then(([users, questions]) => ({
+            users, questions
+        }));
+}
+
+export const saveQuestion = (question) => {
+    return _saveQuestion(question);
+}
+
+export const saveQuestionAnswer = ({ authedUser, qid, answer }) => {
+    return _saveQuestionAnswer({ authedUser, qid, answer });
+}
