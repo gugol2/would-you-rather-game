80ac59580897b603f073bfe80d62e842bec11fd3
jsc <jsanchezcriado@gmail.com>
2020-04-11 01:40:57 +0200

    use the API file instead of the DATA file

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,7 @@
-import { _getUsers, _getQuestions } from '../utils/_DATA';
 import { receiveUsers } from './users';
 import { receiveQuestions } from './questions';
 import { setLoggedUser } from './loggedUser';
+import { getInitialData } from '../utils/api';
 
 export const RECEIVE_DATA = 'RECEIVE_DATA';
 
@@ -9,7 +9,7 @@ const LOGGED_USER = 'johndoe';
 
 export const handleReceiveData = () => {
     return (dispatch) => {
-        Promise.all([_getUsers, _getQuestions]).then(([users, questions]) => {
+        getInitialData.then(({users, questions}) => {
             dispatch(receiveUsers(users));
             dispatch(receiveQuestions(questions));
             dispatch(setLoggedUser(LOGGED_USER))
