2aa2bc29c6e7b0f22afe9585c24985d1ad35b4b4
jsc <jsanchezcriado@gmail.com>
2020-04-11 02:05:13 +0200

    add middleware

---
+++ src/middleware/logger.js
@@ -0,0 +1,8 @@
+export const logger = (store) => (next) => (action) => {
+    console.group(action.type);
+        const result = next(action);
+        console.log('The new state: ', store.getState());
+    console.groupEnd();
+
+    return result;
+}
