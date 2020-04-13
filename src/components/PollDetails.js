4196a6f12ff52012b3671c199689dbf00a45214f
jsc <jsanchezcriado@gmail.com>
2020-04-14 00:13:32 +0200

    get the 404 page if trying to access a question when being unloged

--- src/components/PollDetails.js
+++ src/components/PollDetails.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { ConnectedPoll } from './Poll';
 import { ConnectedPollResults } from './PollResults';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Redirect } from 'react-router-dom';
 
 export const PollDetails = () => {
     let location = useLocation();
     const { state } = location;
-    const { qid, unAnswered, authedUser } = state;
 
-    if(authedUser) {
-        return (
-            <div>
-                {unAnswered ? 
-                    <ConnectedPoll qid={qid} /> 
-                    : 
-                    <ConnectedPollResults qid={qid} />}
-            </div>
-        )
+    if(state) {
+        const { qid, unAnswered, authedUser } = state;
+
+        if(authedUser) {
+            return (
+                <div>
+                    {unAnswered ? 
+                        <ConnectedPoll qid={qid} /> 
+                        : 
+                        <ConnectedPollResults qid={qid} />}
+                </div>
+            )
+        } else {
+            return null;
+        }
     } else {
-        return null;
+        return (
+            <Redirect
+                to={{
+                    pathname: "/404",
+                }}
+            />
+        )
     }
+
 }
