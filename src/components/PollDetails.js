415758a4315c39b48cac6987b9e69e59e95e9433
jsc <jsanchezcriado@gmail.com>
2020-04-15 19:19:10 +0200

    shows a 404 page if a poll does not exist

--- src/components/PollDetails.js
+++ src/components/PollDetails.js
@@ -2,24 +2,29 @@ import React from 'react';
 import { ConnectedUnansweredPoll } from './UnansweredPoll';
 import { ConnectedAnsweredPoll } from './AnsweredPoll';
 import { connect } from 'react-redux';
+import { NoMatch } from './NoMatch';
 
 const PollDetails = ({answered, qid}) => {
-    return (
-        <div>
-            {answered ? 
-                <ConnectedAnsweredPoll qid={qid} />
-                :
-                <ConnectedUnansweredPoll qid={qid}/> 
-            }
-        </div>
-        
-    )
+    if (qid) {
+        return (
+            <div>
+                {answered ? 
+                    <ConnectedAnsweredPoll qid={qid} />
+                    :
+                    <ConnectedUnansweredPoll qid={qid}/> 
+                }
+            </div>
+            
+        )
+    }
+
+    return (<NoMatch />)
 }
 
 const mapStateToProps = ({ authedUser, questions }, { match }) => {
     const { params } = match
-    const question = questions[params.question_id];
-    const answered = question.optionOne.votes.includes(authedUser) || question.optionTwo.votes.includes(authedUser);
+    const question = questions[params.question_id] || {};
+    const answered = question.id && (question.optionOne.votes.includes(authedUser) || question.optionTwo.votes.includes(authedUser));
 
     return {
         qid: question.id,
--- src/components/UnansweredPoll.js
+++ src/components/UnansweredPoll.js
@@ -3,13 +3,12 @@ import { connect } from 'react-redux';
 import { PollHeader } from './PollHeader';
 import { AvatarImage } from './AvatarImage';
 import { handleSaveAnswerToQuestion } from '../actions/questions';
-import { Redirect } from 'react-router-dom';
 
 const UnansweredPoll = (props) => {
     const [selectedOption, setSelectedOption] = useState('optionOne');
 
     const { question, pollAuthor, dispatch, authedUser } = props;
-    const { optionOne, optionTwo, id } = question;
+    const { optionOne, optionTwo } = question;
     
     const handleChange = (event) => {
         setSelectedOption(event.target.value)
@@ -21,53 +20,47 @@ const UnansweredPoll = (props) => {
         dispatch(handleSaveAnswerToQuestion({ authedUser, qid: question.id, answer: selectedOption}));
     }
     
-    if(id) {
-        return (
-            <div className='poll'>
-                <PollHeader author={pollAuthor}/>
-    
-                <div className='poll__body'>
-                    <div className="poll__left">
-                        <AvatarImage user={pollAuthor}/>
-                    </div>
-    
-                    <form className='poll__question' onSubmit={saveQuestionAnswer} >
-                        <strong>Would your rather</strong>
-                        <label>
-                            <input 
-                                type="radio" 
-                                value='optionOne' 
-                                checked={selectedOption === 'optionOne'}
-                                onChange={handleChange}
-                                />
-                            {optionOne && optionOne.text}
-                        </label>
-    
-                        <label>
-                            <input
-                                type="radio"
-                                value='optionTwo'
-                                checked={selectedOption === 'optionTwo'}
-                                onChange={handleChange}
-                                />
-                            {optionTwo && optionTwo.text}
-                        </label>
-    
-                        <button 
-                            type="submit"
-                            className='center'
-                        >Submit</button>
-                    </form>
-    
+    return (
+        <div className='poll'>
+            <PollHeader author={pollAuthor}/>
+
+            <div className='poll__body'>
+                <div className="poll__left">
+                    <AvatarImage user={pollAuthor}/>
                 </div>
-    
+
+                <form className='poll__question' onSubmit={saveQuestionAnswer} >
+                    <strong>Would your rather</strong>
+                    <label>
+                        <input 
+                            type="radio" 
+                            value='optionOne' 
+                            checked={selectedOption === 'optionOne'}
+                            onChange={handleChange}
+                            />
+                        {optionOne && optionOne.text}
+                    </label>
+
+                    <label>
+                        <input
+                            type="radio"
+                            value='optionTwo'
+                            checked={selectedOption === 'optionTwo'}
+                            onChange={handleChange}
+                            />
+                        {optionTwo && optionTwo.text}
+                    </label>
+
+                    <button 
+                        type="submit"
+                        className='center'
+                    >Submit</button>
+                </form>
+
             </div>
-        )
-    } else {
-        return (
-            <Redirect to='/404' />
-        );
-    }
+
+        </div>
+    )
 
 }
 
