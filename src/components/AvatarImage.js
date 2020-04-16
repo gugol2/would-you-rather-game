f8dd483819c90b1870a615912ddfd9a1371ebc37
jsc <jsanchezcriado@gmail.com>
2020-04-16 14:01:24 +0200

    modified data to make this game nicer

--- src/components/AvatarImage.js
+++ src/components/AvatarImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const AvatarImage = ({user, modifier}) => {
     return(
         <img 
-            src={ user ? user.avatarURL : `${process.env.PUBLIC_URL}/images/default_avatar.png` } 
+            src={ user ? user.avatarURL : `${process.env.PUBLIC_URL}/images/default_avatar.jpg` } 
             alt={ user ? user.name : 'default user' }
             className={ modifier ? `avatar-img avatar-img--${modifier}` : 'avatar-img' }
         />
--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,10 +1,10 @@
 const imagesPath = process.env.PUBLIC_URL + '/images/';
 
 let users = {
-  sarahedo: {
-    id: 'sarahedo',
-    name: 'Sarah Edo',
-    avatarURL: `${imagesPath}girl.jpg`,
+  catwoman: {
+    id: 'catwoman',
+    name: 'Cat-Woman',
+    avatarURL: `${imagesPath}catwoman.jpg`,
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -13,20 +13,20 @@ let users = {
     },
     questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
   },
-  tylermcginnis: {
-    id: 'tylermcginnis',
-    name: 'Tyler McGinnis',
-    avatarURL: `${imagesPath}another_ninja.jpg`,
+  batman: {
+    id: 'batman',
+    name: 'Bat-Man',
+    avatarURL: `${imagesPath}batman.jpg`,
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
     },
     questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
   },
-  johndoe: {
-    id: 'johndoe',
-    name: 'John Doe',
-    avatarURL: `${imagesPath}ninja.jpg`,
+  superman: {
+    id: 'superman',
+    name: 'Superman',
+    avatarURL: `${imagesPath}superman.jpg`,
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
@@ -39,10 +39,10 @@ let users = {
 let questions = {
   "8xf0y6ziyjabvozdd253nd": {
     id: '8xf0y6ziyjabvozdd253nd',
-    author: 'sarahedo',
+    author: 'catwoman',
     timestamp: 1467166872634,
     optionOne: {
-      votes: ['sarahedo'],
+      votes: ['catwoman'],
       text: 'have horrible short term memory',
     },
     optionTwo: {
@@ -52,66 +52,66 @@ let questions = {
   },
   "6ni6ok3ym7mf1p33lnez": {
     id: '6ni6ok3ym7mf1p33lnez',
-    author: 'johndoe',
+    author: 'superman',
     timestamp: 1468479767190,
     optionOne: {
       votes: [],
       text: 'become a superhero',
     },
     optionTwo: {
-      votes: ['johndoe', 'sarahedo'],
+      votes: ['superman', 'catwoman'],
       text: 'become a supervillain'
     }
   },
   "am8ehyc8byjqgar0jgpub9": {
     id: 'am8ehyc8byjqgar0jgpub9',
-    author: 'sarahedo',
+    author: 'catwoman',
     timestamp: 1488579767190,
     optionOne: {
       votes: [],
       text: 'be telekinetic',
     },
     optionTwo: {
-      votes: ['sarahedo'],
+      votes: ['catwoman'],
       text: 'be telepathic'
     }
   },
   "loxhs1bqm25b708cmbf3g": {
     id: 'loxhs1bqm25b708cmbf3g',
-    author: 'tylermcginnis',
+    author: 'batman',
     timestamp: 1482579767190,
     optionOne: {
       votes: [],
       text: 'be a front-end developer',
     },
     optionTwo: {
-      votes: ['sarahedo'],
+      votes: ['catwoman'],
       text: 'be a back-end developer'
     }
   },
   "vthrdm985a262al8qx3do": {
     id: 'vthrdm985a262al8qx3do',
-    author: 'tylermcginnis',
+    author: 'batman',
     timestamp: 1489579767190,
     optionOne: {
-      votes: ['tylermcginnis'],
+      votes: ['batman'],
       text: 'find $50 yourself',
     },
     optionTwo: {
-      votes: ['johndoe'],
+      votes: ['superman'],
       text: 'have your best friend find $500'
     }
   },
   "xj352vofupe1dqz9emx13r": {
     id: 'xj352vofupe1dqz9emx13r',
-    author: 'johndoe',
+    author: 'superman',
     timestamp: 1493579767190,
     optionOne: {
-      votes: ['johndoe'],
+      votes: ['superman'],
       text: 'write JavaScript',
     },
     optionTwo: {
-      votes: ['tylermcginnis'],
+      votes: ['batman'],
       text: 'write Swift'
     }
   },
