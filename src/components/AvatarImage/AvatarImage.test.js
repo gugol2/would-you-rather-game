696152bc69896fd7073b886a64adc6e457223f42
jsc <jsanchezcriado@gmail.com>
2020-04-18 11:27:23 +0200

    getting ready for tests

---
+++ src/components/AvatarImage/AvatarImage.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import { AvatarImage } from './AvatarImage';
+
+// test('renders learn react link', () => {
+//   const { container, getByText, debug } = render(<AvatarImage />);
+//   debug();
+//   console.log('container', container.firstChild.toHaveAttribute('src', 'something'));
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
