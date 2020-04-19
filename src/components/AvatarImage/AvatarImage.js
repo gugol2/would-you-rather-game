719e579c5bdde2561dfcc5db63522097c63bd79a
jsc <jsanchezcriado@gmail.com>
2020-04-19 23:14:13 +0200

    added first test with Jest

--- src/components/AvatarImage/AvatarImage.js
+++ src/components/AvatarImage/AvatarImage.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 export const AvatarImage = ({user, modifier}) => {
 	return(
 		<img 
+			data-testid="avatar-img"
 			// eslint-disable-next-line no-undef
 			src={ user ? user.avatarURL : `${process.env.PUBLIC_URL}/images/default_avatar.jpg` } 
 			alt={ user ? user.name : 'default user' }
--- src/components/AvatarImage/AvatarImage.test.js
+++ src/components/AvatarImage/AvatarImage.test.js
@@ -1,11 +1,8 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import { AvatarImage } from './AvatarImage';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { AvatarImage } from './AvatarImage';
 
-// test('renders learn react link', () => {
-//   const { container, getByText, debug } = render(<AvatarImage />);
-//   debug();
-//   console.log('container', container.firstChild.toHaveAttribute('src', 'something'));
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+	const {getByTestId} = render(<AvatarImage />);
+	expect(getByTestId('avatar-img')).toBeInTheDocument();
+});
