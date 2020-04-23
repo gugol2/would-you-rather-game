a3b045262e89379fe7ef81c166d46dcce79cf971
jsc <jsanchezcriado@gmail.com>
2020-04-23 11:18:40 +0200

    use the wrapper option of '@testing-library/react' to wrap NavBar into the BrowserRouter

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { NavBar } from './NavBar';
 
 test('renders NavBar', () => {
-  const { getByTestId } = render(
-    <Router>
-      <NavBar />
-    </Router>,
-  );
+  const { getByTestId } = render(<NavBar />, { wrapper: BrowserRouter });
   const navBarRendered = getByTestId('nav-bar');
   expect(navBarRendered).toBeInTheDocument();
   expect(navBarRendered).toHaveClass('navbar');
