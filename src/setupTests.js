d5a2acedb5132149a31a3bfdcac76ad9aa21ffa4
jsc <jsanchezcriado@gmail.com>
2020-04-28 20:32:01 +0200

    move test helper files to its own folder

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-import { renderWithRouter } from './renderWithRouter';
-import { renderWithProvider } from './renderWithProvider';
+import { renderWithRouter } from './testHelpers/renderWithRouter';
+import { renderWithProvider } from './testHelpers/renderWithProvider';
 
 export { renderWithRouter, renderWithProvider };
