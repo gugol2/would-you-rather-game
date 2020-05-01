99fffe2f2c0b48726ffb139869883b1e9a0d952d
jsc <jsanchezcriado@gmail.com>
2020-05-01 23:18:50 +0200

    added test for the shared action file

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,12 +3,11 @@ import { receiveQuestions } from './questions';
 import { getInitialData } from '../utils/api';
 import { showLoading, hideLoading } from 'react-redux-loading';
 
-export const RECEIVE_DATA = 'RECEIVE_DATA';
-
 export const handleReceiveData = () => {
   return dispatch => {
     dispatch(showLoading());
-    getInitialData().then(({ users, questions }) => {
+
+    return getInitialData().then(({ users, questions }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveQuestions(questions));
       dispatch(hideLoading());
---
+++ src/actions/shared.test.js
@@ -0,0 +1,57 @@
+import { handleReceiveData } from './shared';
+import { getInitialData as mockedGetInitialData } from '../utils/api';
+import { RECEIVE_USERS } from '../actions/users';
+import { RECEIVE_QUESTIONS } from '../actions/questions';
+
+jest.mock('../utils/api');
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('handleReceiveData', () => {
+  const showLoadingAction = {
+    payload: { scope: 'default' },
+    type: 'loading-bar/SHOW',
+  };
+  const hideLoadingAction = {
+    payload: { scope: 'default' },
+    type: 'loading-bar/HIDE',
+  };
+  const dispatch = jest.fn();
+  const users = '::users::';
+  const questions = '::questions::';
+
+  test('should dispatch showLoading, receiveUsers, receiveQuestions and hideLoading in that order when the API succeeds', async () => {
+    mockedGetInitialData.mockImplementation(() =>
+      Promise.resolve({ users, questions }),
+    );
+
+    const receiveData = handleReceiveData();
+
+    await receiveData(dispatch);
+
+    expect(dispatch).toHaveBeenCalledTimes(4);
+    expect(dispatch).toHaveBeenNthCalledWith(1, showLoadingAction);
+    expect(dispatch).toHaveBeenNthCalledWith(2, {
+      type: RECEIVE_USERS,
+      users,
+    });
+    expect(dispatch).toHaveBeenNthCalledWith(3, {
+      type: RECEIVE_QUESTIONS,
+      questions,
+    });
+    expect(dispatch).toHaveBeenNthCalledWith(4, hideLoadingAction);
+  });
+
+  test('should dispatch ONLY showLoading when the API fails', async () => {
+    mockedGetInitialData.mockImplementation(() => Promise.reject('API failed'));
+
+    const receiveData = handleReceiveData();
+
+    await receiveData(dispatch).catch(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenCalledWith(showLoadingAction);
+    });
+  });
+});
