e59462de96ac4dd0bc4dd9a6d09701b8249732eb
jsc <jsanchezcriado@gmail.com>
2020-06-08 23:47:44 +0200

    typed autheduser actions creator

--- src/actions/authedUser.ts
+++ src/actions/authedUser.ts
@@ -1,14 +1,19 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER';
 export const LOGOUT_AUTHED_USER = 'LOGOUT_AUTHED_USER';
 
-export const setAuthedUser = (id: string) => {
+interface AuthedUserAction {
+  type: string;
+  id?: string;
+}
+
+export const setAuthedUser = (id: string): AuthedUserAction => {
   return {
     type: SET_AUTHED_USER,
     id,
   };
 };
 
-export const logOutAuthedUser = () => {
+export const logOutAuthedUser = (): AuthedUserAction => {
   return {
     type: LOGOUT_AUTHED_USER,
   };
