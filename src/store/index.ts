6dab8b0ca78266038e8daafb895bf1eec2f4f8dc
jsc <jsanchezcriado@gmail.com>
2020-06-11 14:46:45 +0200

    [TS] moved AppDispatch type with the rest of types

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,4 +3,3 @@ import { rootReducer } from '../reducers';
 import { buildMiddleware } from '../middleware';
 
 export const store = createStore(rootReducer, buildMiddleware());
-export type AppDispatch = typeof store.dispatch;
