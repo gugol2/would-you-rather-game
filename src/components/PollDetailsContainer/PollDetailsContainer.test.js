f8e80309b9f76cfa1b956d973c7a8dbaa75a9c1f
gugol2 <jsanchezcriado@gmail.com>
2025-09-22 22:01:57 +0200

    [FIX] fix PollDetailsContainer test according to new RTL methods

--- src/components/PollDetailsContainer/PollDetailsContainer.test.js
+++ src/components/PollDetailsContainer/PollDetailsContainer.test.js
@@ -1,20 +1,27 @@
 import { render } from '@testing-library/react';
 import { PollDetailsContainer } from './PollDetailsContainer';
 import React from 'react';
-import { ConnectedUnansweredPoll as MockedUnansweredPoll } from '../UnansweredPoll';
-import { ConnectedAnsweredPoll as MockedAnsweredPoll } from '../AnsweredPoll';
-import { NoMatch as MockedNoMatch } from '../NoMatch';
+
+/* eslint-disable react/prop-types */
 
 jest.mock('../UnansweredPoll', () => ({
-  ConnectedUnansweredPoll: jest.fn(() => <>Mocked Unanswered Poll</>),
+  ConnectedUnansweredPoll: ({ qid }) => (
+    <div data-testid='mocked-unanswered-poll' data-qid={qid}>
+      Mocked Unanswered Poll{' '}
+    </div>
+  ),
 }));
 
 jest.mock('../AnsweredPoll', () => ({
-  ConnectedAnsweredPoll: jest.fn(() => <>Mocked Answered Poll</>),
+  ConnectedAnsweredPoll: ({ qid }) => (
+    <div data-testid='mocked-answered-poll' data-qid={qid}>
+      Mocked Answered Poll
+    </div>
+  ),
 }));
 
 jest.mock('../NoMatch', () => ({
-  NoMatch: jest.fn(() => <>Mocked NoMatch</>),
+  NoMatch: () => <div data-testid='mocked-no-match'>Mocked NoMatch</div>,
 }));
 
 afterEach(() => {
@@ -22,25 +29,28 @@ afterEach(() => {
 });
 
 describe('PollDetailsContainer', () => {
-  const context = {};
   let answered = undefined;
   let qid = undefined;
 
-  test.skip('should render NoMatch when the qid is falsy', () => {
+  test('should render NoMatch when the qid is falsy', () => {
     const props = {
       answered,
       qid,
     };
 
-    render(<PollDetailsContainer {...props} />);
+    const { queryByTestId } = render(<PollDetailsContainer {...props} />);
 
-    expect(MockedUnansweredPoll).not.toHaveBeenCalled();
-    expect(MockedAnsweredPoll).not.toHaveBeenCalled();
-    expect(MockedNoMatch).toHaveBeenCalledTimes(1);
-    expect(MockedNoMatch).toHaveBeenCalledWith({}, context);
+    const MockedUnansweredPoll = queryByTestId('mocked-unanswered-poll');
+    expect(MockedUnansweredPoll).not.toBeInTheDocument();
+    const MockedAnsweredPoll = queryByTestId('mocked-answered-poll');
+    expect(MockedAnsweredPoll).not.toBeInTheDocument();
+
+    const MockedNoMatch = queryByTestId('mocked-no-match');
+    expect(MockedNoMatch).toBeInTheDocument();
+    expect(MockedNoMatch).toHaveTextContent('Mocked NoMatch');
   });
 
-  test.skip('should render UnansweredPoll when the qid is truthy and answered is falsy', () => {
+  test('should render UnansweredPoll when the qid is truthy and answered is falsy', () => {
     qid = '::qid::';
     answered = false;
     const props = {
@@ -48,15 +58,20 @@ describe('PollDetailsContainer', () => {
       qid,
     };
 
-    render(<PollDetailsContainer {...props} />);
+    const { queryByTestId } = render(<PollDetailsContainer {...props} />);
 
-    expect(MockedUnansweredPoll).toHaveBeenCalledTimes(1);
-    expect(MockedUnansweredPoll).toHaveBeenCalledWith({ qid }, context);
-    expect(MockedAnsweredPoll).not.toHaveBeenCalled();
-    expect(MockedNoMatch).not.toHaveBeenCalled();
+    const MockedUnansweredPoll = queryByTestId('mocked-unanswered-poll');
+    expect(MockedUnansweredPoll).toBeInTheDocument();
+    expect(MockedUnansweredPoll).toHaveAttribute('data-qid', qid);
+
+    const MockedAnsweredPoll = queryByTestId('mocked-answered-poll');
+    expect(MockedAnsweredPoll).not.toBeInTheDocument();
+
+    const MockedNoMatch = queryByTestId('mocked-no-match');
+    expect(MockedNoMatch).not.toBeInTheDocument();
   });
 
-  test.skip('should render AnsweredPoll when the qid and answered are truthy', () => {
+  test('should render AnsweredPoll when the qid and answered are truthy', () => {
     qid = '::qid::';
     answered = true;
     const props = {
@@ -64,11 +79,16 @@ describe('PollDetailsContainer', () => {
       qid,
     };
 
-    render(<PollDetailsContainer {...props} />);
+    const { queryByTestId } = render(<PollDetailsContainer {...props} />);
 
-    expect(MockedAnsweredPoll).toHaveBeenCalledTimes(1);
-    expect(MockedAnsweredPoll).toHaveBeenCalledWith({ qid }, context);
-    expect(MockedUnansweredPoll).not.toHaveBeenCalled();
-    expect(MockedNoMatch).not.toHaveBeenCalled();
+    const MockedAnsweredPoll = queryByTestId('mocked-answered-poll');
+    expect(MockedAnsweredPoll).toBeInTheDocument();
+    expect(MockedAnsweredPoll).toHaveAttribute('data-qid', qid);
+
+    const MockedUnansweredPoll = queryByTestId('mocked-unanswered-poll');
+    expect(MockedUnansweredPoll).not.toBeInTheDocument();
+
+    const MockedNoMatch = queryByTestId('mocked-no-match');
+    expect(MockedNoMatch).not.toBeInTheDocument();
   });
 });
