5ea9af19616779cc75d4a5dff5e2756f435e7223
gugol2 <jsanchezcriado@gmail.com>
2025-09-22 20:01:31 +0200

    [FIX] fix App test according to new RTL methods

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,77 +1,127 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
 import { App } from './App';
-import * as MockedLoadingBar from 'react-redux-loading';
-import { BrowserRouter as MockedBrowserRouter } from 'react-router-dom';
-import { ConnectedLoggedUserInfo as MockedConnectedLoggedUserInfo } from '../LoggedUserInfo';
-import { NavBar as MockdedNavBar } from '../NavBar';
-import { UnauthenticatedApp as MockedUnauthenticatedApp } from './UnauthenticatedApp';
-import { AuthenticatedApp as MockedAuthenticatedApp } from './AuthenticatedApp';
+
+/* eslint-disable react/prop-types */
 
 jest.mock('react-router-dom', () => ({
-  BrowserRouter: jest.fn(({ children }) => <>{children}</>),
+  BrowserRouter: ({ children }) => (
+    <div data-testid='mocked-browser-router'>{children}</div>
+  ),
 }));
 
 jest.mock('../NavBar', () => ({
-  NavBar: jest.fn(() => <>Mockded NavBar</>),
+  NavBar: () => <div data-testid='mocked-nav-bar'>Mockded NavBar</div>,
 }));
 
 jest.mock('../LoggedUserInfo', () => ({
-  ConnectedLoggedUserInfo: jest.fn(() => <>Mocked LoggedUserInfo</>),
+  ConnectedLoggedUserInfo: () => (
+    <div data-testid='mocked-logged-user-info'>
+      Mocked ConnectedLoggedUserInfo
+    </div>
+  ),
 }));
 
-jest.mock('react-redux-loading', () => jest.fn(() => <>Mocked LoadingBar</>));
+jest.mock('react-redux-loading', () => {
+  const MockLoadingBar = ({ className }) => (
+    <div data-testid='mocked-loading-bar' className={className}>
+      Mocked LoadingBar
+    </div>
+  );
+  MockLoadingBar.displayName = 'MockLoadingBar';
+  return MockLoadingBar;
+});
 
 jest.mock('./UnauthenticatedApp', () => ({
-  UnauthenticatedApp: jest.fn(() => <>Mocked UnauthenticatedApp</>),
+  UnauthenticatedApp: () => (
+    <div data-testid='mocked-unauthenticated-app'>
+      Mocked UnauthenticatedApp
+    </div>
+  ),
 }));
 
 jest.mock('./AuthenticatedApp', () => ({
-  AuthenticatedApp: jest.fn(() => <>Mocked AuthenticatedApp</>),
+  AuthenticatedApp: () => (
+    <div data-testid='mocked-authenticated-app'>Mocked AuthenticatedApp</div>
+  ),
 }));
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
-const context = {};
+test('should render the common components no matter the authedUser ', () => {
+  const { getByTestId, queryAllByTestId } = render(<App />);
 
-test.skip('should render the common components no matter the authedUser ', () => {
-  render(<App />);
+  const MockedBrowserRouter = getByTestId('mocked-browser-router');
+  expect(MockedBrowserRouter).toBeInTheDocument();
 
-  expect(MockedBrowserRouter).toHaveBeenCalledTimes(1);
+  const MockdedNavBar = queryAllByTestId('mocked-nav-bar');
+  expect(MockdedNavBar).toHaveLength(1);
+  expect(MockdedNavBar[0]).toHaveTextContent('Mockded NavBar');
 
-  expect(MockdedNavBar).toHaveBeenCalledTimes(1);
-  expect(MockdedNavBar).toHaveBeenCalledWith({}, context);
-
-  expect(MockedLoadingBar).toHaveBeenCalledTimes(1);
-  expect(MockedLoadingBar).toHaveBeenCalledWith(
-    { className: 'loading-bar' },
-    context,
+  const MockedUnauthenticatedApp = queryAllByTestId(
+    'mocked-unauthenticated-app',
   );
+  expect(MockedUnauthenticatedApp).toHaveLength(1);
+
+  const MockedLoadingBar = queryAllByTestId('mocked-loading-bar');
+  expect(MockedLoadingBar).toHaveLength(1);
+  expect(MockedLoadingBar[0]).toHaveTextContent('Mocked LoadingBar');
+  expect(MockedLoadingBar[0]).toHaveClass('loading-bar');
 });
 
-test.skip('should render the UnauthenticatedApp when authedUser is falsy', () => {
+test('should render the UnauthenticatedApp when authedUser is falsy but not LoggedUserInfo and the AuthenticatedApp', () => {
   const authedUser = null;
-  render(<App authedUser={authedUser} />);
+  const { queryAllByTestId } = render(<App authedUser={authedUser} />);
 
-  expect(MockedConnectedLoggedUserInfo).not.toHaveBeenCalled();
+  const MockedConnectedLoggedUserInfo = queryAllByTestId(
+    'mocked-logged-user-info',
+  );
+  expect(MockedConnectedLoggedUserInfo).toHaveLength(0);
 
-  expect(MockedUnauthenticatedApp).toHaveBeenCalledTimes(1);
-  expect(MockedUnauthenticatedApp).toHaveBeenCalledWith({}, context);
+  const MockedLoadingBar = queryAllByTestId('mocked-loading-bar');
+  expect(MockedLoadingBar).toHaveLength(1);
+  expect(MockedLoadingBar[0]).toHaveTextContent('Mocked LoadingBar');
+  expect(MockedLoadingBar[0]).toHaveClass('loading-bar');
 
-  expect(MockedAuthenticatedApp).not.toHaveBeenCalled();
+  const MockedUnauthenticatedApp = queryAllByTestId(
+    'mocked-unauthenticated-app',
+  );
+  expect(MockedUnauthenticatedApp).toHaveLength(1);
+  expect(MockedUnauthenticatedApp[0]).toHaveTextContent(
+    'Mocked UnauthenticatedApp',
+  );
+
+  const MockedAuthenticatedApp = queryAllByTestId('mocked-authenticated-app');
+  expect(MockedAuthenticatedApp).toHaveLength(0);
 });
 
-test.skip('should render the LoggedUserInfo and the AuthenticatedApp when authedUser is truthy', () => {
+test('should render the LoggedUserInfo and the AuthenticatedApp when authedUser is truthy but not UnauthenticatedApp', () => {
   const authedUser = '::authedUser::';
-  render(<App authedUser={authedUser} />);
+  const { queryAllByTestId } = render(<App authedUser={authedUser} />);
 
-  expect(MockedConnectedLoggedUserInfo).toHaveBeenCalledTimes(1);
-  expect(MockedConnectedLoggedUserInfo).toHaveBeenCalledWith({}, context);
+  const MockedConnectedLoggedUserInfo = queryAllByTestId(
+    'mocked-logged-user-info',
+  );
+  expect(MockedConnectedLoggedUserInfo).toHaveLength(1);
+  expect(MockedConnectedLoggedUserInfo[0]).toHaveTextContent(
+    'Mocked ConnectedLoggedUserInfo',
+  );
 
-  expect(MockedAuthenticatedApp).toHaveBeenCalledTimes(1);
-  expect(MockedAuthenticatedApp).toHaveBeenCalledWith({}, context);
+  const MockedLoadingBar = queryAllByTestId('mocked-loading-bar');
+  expect(MockedLoadingBar).toHaveLength(1);
+  expect(MockedLoadingBar[0]).toHaveTextContent('Mocked LoadingBar');
+  expect(MockedLoadingBar[0]).toHaveClass('loading-bar');
 
-  expect(MockedUnauthenticatedApp).not.toHaveBeenCalled();
+  const MockedAuthenticatedApp = queryAllByTestId('mocked-authenticated-app');
+  expect(MockedAuthenticatedApp).toHaveLength(1);
+  expect(MockedAuthenticatedApp[0]).toHaveTextContent(
+    'Mocked AuthenticatedApp',
+  );
+
+  const MockedUnauthenticatedApp = queryAllByTestId(
+    'mocked-unauthenticated-app',
+  );
+  expect(MockedUnauthenticatedApp).toHaveLength(0);
 });
