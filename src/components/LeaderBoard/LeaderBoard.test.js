b2aac837bb22d709a71082e0ca5c525f47cb6d14
gugol2 <jsanchezcriado@gmail.com>
2025-09-22 15:09:27 +0200

    pick the mocked Component with a more as-the-user-would-do-approach

--- src/components/LeaderBoard/LeaderBoard.test.js
+++ src/components/LeaderBoard/LeaderBoard.test.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { LeaderBoard } from './LeaderBoard';
 import { render } from '@testing-library/react';
 
-const MockAvatarImage = jest.fn(() => <>MockedAvatarImage</>);
-
 jest.mock('../AvatarImage', () => ({
   AvatarImage: ({ user, size }) => {
-    MockAvatarImage({ user, size });
-
     return (
       <div
         data-testid='avatar-image'
@@ -39,7 +35,8 @@ test('renders the LeaderBoard with one user', () => {
   const answer = {};
   const answers = { answer };
   const questions = [];
-  const user = { answers, questions, id };
+  const name = '::name::';
+  const user = { answers, questions, id, name };
   const usersOrderedByScore = [user];
   const { getByTestId, getAllByTestId } = render(
     <LeaderBoard usersOrderedByScore={usersOrderedByScore} />,
@@ -48,8 +45,10 @@ test('renders the LeaderBoard with one user', () => {
   const leaderBoardItem = getAllByTestId('leaderboarditem');
   expect(leaderBoardItem.length).toBe(1);
 
-  expect(MockAvatarImage).toHaveBeenCalledTimes(1);
-  expect(MockAvatarImage).toHaveBeenCalledWith({ user, size: 'medium' });
+  const AvatarImageComponent = getByTestId('avatar-image');
+  expect(AvatarImageComponent).toBeInTheDocument();
+  expect(AvatarImageComponent).toHaveAttribute('data-user-name', name);
+  expect(AvatarImageComponent).toHaveAttribute('data-size', 'medium');
 
   const answeredQuestions = getByTestId('answeredquestions');
   expect(answeredQuestions).toHaveTextContent(Object.keys(answers).length);
